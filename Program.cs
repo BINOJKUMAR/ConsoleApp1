674e373042742544b7c2c25bbb79807081a35e4c
BinojKumarGopinathan <37065280+BinojKumarGopinathan@users.noreply.github.com>
2025-08-25 15:21:47 +0400

    Revert "Revert "Test2""
    This reverts commit c9612a264a5cbe00ba1fbde9667b5f7daad1bf66.

--- Program.cs
+++ Program.cs
@@ -5,3 +5,11 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
